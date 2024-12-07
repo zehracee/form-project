c5854f639688e24b9a69bc62933d9ef97ef30640
MyRepo <166295758+cmpe-dev@users.noreply.github.com>
2024-12-07 08:57:06 +0300

    script.js

--- script.js
+++ script.js
@@ -1,21 +1,50 @@
  
 const abstracts = [
     {
-        abstract: "Eloksallı ve anodize edilmemiş Ti6Al4V ve Mg AZ31B biyomalzemelerinin korozyon davranışına ilişkin uzun vadeli bir araştırma, kontrollü koşullar altında gerçekleştirilmiştir. Ti6Al4V fosforik asit ve oksalik asit içinde anodize edilirken, Mg AZ31B fosforik asit ve potasyum hidroksit içinde 20V DC potansiyel uygulanarak anodize edilmiştir. %3 NaCl, fosfat tamponlu salin (PBS) ve deiyonize (DI) su çözeltilerine daldırılarak uzun süreli deneyler yapıldı. Korozyon hızı ve deseni elektrokimyasal analiz ile ölçülmüş ve anotlamanın malzeme yüzeyindeki doğal oksit tabakasını arttırdığı, korozyon hızını azalttığı ve biyomalzemenin ömrünü uzattığı gözlemlenmiştir.",
+        abstract: "Bu çalışmada 1980-2020 yılları arasında Konya ilinin aylık yağış, nem ve sıcaklık verileri, buğday üretim miktarı ve buğday verimlilik verileri kullanılmıştır. Bu veriler kullanılarak Recurrent Neural Network (RNN) tabanlı algoritmalar olan (Gated Recurrent Units) GRU ve Long Short Term Memory (LSTM) yöntemleri ile buğday verimlilik tahmini yapılmıştır. Gerçekleştirilen GRU tabanlı model ile buğday verimliliği tahmin performansları incelendiğinde R2 puan, MSE, RMSE, MAE ve MAPE değerleri için sırasıyla 0.9550, 0.0059, 0.0280, 0.0623, 7.45 değerleri elde edilmiştir. RNN tabanlı bir diğer yöntem olan LSTM yöntemiyle elde edilen performans sonuçlarında ise R2 puan, MSE, RMSE, MAE ve MAPE değerleri için sırasıyla 0.9667, 0.0054, 0.0280, 0.0614, 7.33 değerleri elde edilmiştir. LSTM yöntemi, GRU yönteminden daha iyi sonuçlar vermesine rağmen LSTM yönteminin eğitim modelleme süresi GRU yönteminden daha fazla sürmüştür.",
         models: [
-            "GELİŞMİŞ KOROZYON DİRENCİ İÇİN ELOKSALLI METALİK İMPLANTLARIN ELEKTROKİMYASAL DAVRANIŞLARININ İNCELENMESİ",
-            "GELİŞMİŞ KOROZYON DİRENCİ İÇİN ELOKSALLI METALİK İMPLANTLARIN ELEKTROKİMYASAL DAVRANIŞLARININ İNCELENMESİ",
-            "GELİŞMİŞ KOROZYON DİRENCİ İÇİN ELOKSALLI METALİK İMPLANTLARIN ELEKTROKİMYASAL DAVRANIŞLARININ İNCELENMESİ",
-            "GELİŞMİŞ KOROZYON DİRENCİ İÇİN ELOKSALLI METALİK İMPLANTLARIN ELEKTROKİMYASAL DAVRANIŞLARININ İNCELENMESİ"
+            "Konya İlinde Buğday Verimliliğinin Tahmini: GRU ve LSTM Ağları ile Zaman Serisi Analizi (1980-2020)",
+            "Konya İlinde Buğday Verimliliğinin Tahmininde Gated Recurrent Units (GRU) ve Long Short-Term Memory (LSTM) Modellerinin Karşılaştırılması",
+            "Konya İli Buğday Verimliliği Tahmini: GRU ve LSTM Yöntemleri Karşılaştırması",
+            "Konya Bölgesi için 1980-2020 Döneminde Buğday Verimlilik Tahmini: GRU ve LSTM Yöntemleri ile Karşılaştırması"
         ]
     },
     {
-        abstract: "Eloksallı ve anodize edilmemiş Ti6Al4V ve Mg AZ31B biyomalzemelerinin korozyon davranışına ilişkin uzun vadeli bir araştırma, kontrollü koşullar altında gerçekleştirilmiştir. Ti6Al4V fosforik asit ve oksalik asit içinde anodize edilirken, Mg AZ31B fosforik asit ve potasyum hidroksit içinde 20V DC potansiyel uygulanarak anodize edilmiştir. %3 NaCl, fosfat tamponlu salin (PBS) ve deiyonize (DI) su çözeltilerine daldırılarak uzun süreli deneyler yapıldı. Korozyon hızı ve deseni elektrokimyasal analiz ile ölçülmüş ve anotlamanın malzeme yüzeyindeki doğal oksit tabakasını arttırdığı, korozyon hızını azalttığı ve biyomalzemenin ömrünü uzattığı gözlemlenmiştir.",
+        abstract: "İnternet hayatımızın her alanında önemli bir yere sahip ve önemi her geçen gün artmaya devam ediyor. Bu nedenle internetin kullanılabilirliği büyük önem taşımaktadır. Düşük veri kalitesi ve dezenformasyon, internetin kullanılabilirliğini ciddi şekilde etkilemektedir. Bu nedenle insanlar doğru ve temiz bilgiye ulaşma konusunda zorluklarla karşılaşmaktadır. Günümüzde web sitelerinde metin tabanlı içerik yerine ağırlıklı olarak resim ve video gibi görsel tabanlı içerikler daha çok yer almaktadır. Bu tür içeriklerin sınıflandırılması arama motorları için büyük önem taşımaktadır. Sonuç olarak web sayfalarının sınıflandırılması bilim insanları için önemli bir araştırma alanı olarak karşımıza çıkmaktadır. Bu çalışma görsel tabanlı web sayfalarının sınıflandırılmasına odaklanmaktadır. Web sayfalarını turizm, makine, müzik ve spor olmak üzere dört ana grupta sınıflandırmak için derin öğrenmeye dayalı bir yöntem önerilmiştir. Önerilen yöntem, 0,9737 accuracy, 0,9474 recall, 0,9474 F1-score ve 0,9649 AUC değeriyle en iyi sonuçları Stokastik Gradyan İnişi (SGD) optimizasyon yöntemi ile elde etmiştir. Ayrıca, Derin Öğrenmenin (DL) kullanılması, web sayfası sınıflandırmasında, özellikle WebScreenshots veri kümesinde, mevcut literatürdeki en iyi sonuçların elde edilmesini sağlamıştır",
         models: [
-            "GELİŞMİŞ KOROZYON DİRENCİ İÇİN ELOKSALLI METALİK İMPLANTLARIN ELEKTROKİMYASAL DAVRANIŞLARININ İNCELENMESİ",
-            "GELİŞMİŞ KOROZYON DİRENCİ İÇİN ELOKSALLI METALİK İMPLANTLARIN ELEKTROKİMYASAL DAVRANIŞLARININ İNCELENMESİ",
-            "GELİŞMİŞ KOROZYON DİRENCİ İÇİN ELOKSALLI METALİK İMPLANTLARIN ELEKTROKİMYASAL DAVRANIŞLARININ İNCELENMESİ",
-            "GELİŞMİŞ KOROZYON DİRENCİ İÇİN ELOKSALLI METALİK İMPLANTLARIN ELEKTROKİMYASAL DAVRANIŞLARININ İNCELENMESİ"
+            "Derin Öğrenme ile Görsel Tabanlı Web Sayfalarının Sınıflandırılması: Turizm, Makine, Müzik ve Spor Odaklı Yaklaşım",
+            "Görsel Tabanlı Web Sayfalarının Derin Öğrenme ile Sınıflandırılması",
+            "Derin Öğrenmeyle Görsel Tabanlı Web Sayfası Sınıflandırma Yöntemi ve Performansı",
+            "Deep Learning Tabanlı Web Sayfası Sınıflandırması: Veri Kalitesi ve Doğru Bilgiye Erişim"
+        ]
+    },
+  {
+        abstract: "Alzheimer hastalığı yaygın olarak görülen bir demans türü olup, ilerleyen ve tedavisi bulunmayan nörodejeneratif bir hastalıktır. Hastalığı teşhis edebilmek için birçok görüntüleme tekniği kullanılmaktadır. Bu tekniklerden biri Manyetik Rezonans Görüntüleme (MRG) tekniğidir. Erken teşhis edilmesi hastalığın ilerlemesini yavaşlatmak ve gerekli önlemleri alma konusunda hasta ve ailesi için büyük önem taşımaktadır. Erken ve doğru teşhis için derin öğrenme yöntemleriyle bu konuda destekleyici çalışmalar gerçekleştirilmiştir. Aynı zamanda hastalığın seyrini takip etmek için de derin öğrenme yöntemleri kullanılmaktadır. Bu çalışmada hastalığın teşhisi için AlexNet, MobileNetV2 mimarileri ve ESA (Evrişimsel Sinir Ağları) modeli kullanılarak gerçekleştirilmiştir. Toplamda 6400 adet MR görüntüsü kullanılarak her iki mimariden özellik çıkarma işlemi yapılarak bu özellikler birleştirilmiştir. KBA (Komşuluk Bileşen Analizi) algoritması kullanılarak özellik seçimi yapılmış ve DVM (Destek Vektör Makineleri) ile sınıflandırma işlemi gerçekleştirilmiştir. Çalışılan modelde %100 doğruluk değeri hesaplanmıştır.",
+        models: [
+            "Derin Öğrenme ile Manyetik Rezonans Görüntülemede Alzheimer Hastalığı Teşhisi: AlexNet, MobileNetV2 ve ESA'nın Entegrasyonu",
+            "Alzheimer Hastalığının Teşhisinde Derin Öğrenme Tabanlı Hibrit Yaklaşım",
+            "Alzheimer Teşhisinde Derin Öğrenme: AlexNet ve MobileNetV2 ile %100 Doğruluk",
+            "Alzheimer Hastalığının Teşhisinde Derin Öğrenme Yöntemlerinin Uygulanması: MR Görüntüleri ile Veri Madenciliği"
+        ]
+    },
+ 
+  {
+        abstract: "Günümüzde, bir ürünün fiyatı farklı e-ticaret perakendecileri arasında önemli ölçüde değişmektedir. Bu çalışma, Jsoup ile, modern web veri toplama tekniklerini kullanarak kullanıcı dostu bir fiyat karşılaştırma web sitesi geliştirmeyi amaçlamaktadır. Önerilen platform, tanınmış perakendecilerden veri toplamakta, ürün bilgilerini çıkarmak için verileri analiz etmekte ve gerektiğinde gerçek zamanlı olarak tüketicilere sunmaktadır. Ayrıca, kullanıcılara ürün fiyatlarını belirlemede yardımcı olmak için, platform trend verilerini, ekonomik seçenekleri, birim başına en iyi değeri sunan ürünleri ve yüksek puan alan ürünleri sergilemektedir. Platform, çeşitli ürün özelliklerini titizlikle analiz ederek, SVD algoritması yardımıyla anlamlı kalıpları ve korelasyonları tanımlar. Kullanıcılar hem en uygun fiyatlı ürünü hem de diğer kullanıcıların yorumlarına göre önerilen seçeneği bulabilir. Öte yandan, kullanıcılar bir ürünü pazarın sunduğundan daha yüksek bir fiyata satın almanın duygusal sıkıntısından kaçınabilirler. Sonuç olarak, kullanıcılar platform tarafından sağlanan kapsamlı analizden faydalanarak daha bilinçli satın alma kararları verme yetkisine sahip olurlar. Çalışmanın performansı, farklı perakendecilerden alınan gerçek veriler kullanılarak test edilmiştir. Sonuçlar, sistemimizin endüstriyel çözümlere ve ilgili literatüre kıyasla kabul edilebilir bir doğruluk oranına ulaştığını göstermektedir.",
+        models: [
+            "Jsoup ile Yapay Zeka Destekli: Gerçek Zamanlı Fiyat Karşılaştırması ve Bilinçli Satın Alma için Kullanıcı Dostu E-Ticaret Platformu",
+            "Kullanıcı Dostu Fiyat Karşılaştırma Web Sitesi: Jsoup ile Modern Veri Toplama",
+            "Modern Web Veri Toplama ile Kullanıcı Dostu Fiyat Karşılaştırma Platformu Geliştirme",
+            "Uygun Fiyatlı Ürün Bulma Platformu: Jsoup ile Web Veri Analizi ve Fiyat Karşılaştırması"
+        ]
+    },
+ 
+  {
+        abstract: "Orman yangınları dünyada sıkça rastlanan bir doğal afettir. Orman yangınları sonrası yanan alanların belirlenmesinde uzaktan algılama teknolojileri sıklıkla kullanılmaktadır. Bu çalışmada 18 Ağustos 2019 tarihinde İzmir ili Karabağlar ilçesinde meydana gelen ve 53 saat süren orman yangını incelenmiştir. Multispektral görüntüler (MSI) ve Yapay Açıklıklı Radar (SAR) veri setlerine (Landsat 8, Sentinel 2 ve Sentinel 1A) uzaktan algılama teknikleri uygulanmıştır. Çalışma bölgesine ait orman yangını öncesi sırasıyla Landsat 8 ve Sentinel 2 için 16 ve 11 Ağustos 2019 ve orman yangını sonrası 23 ve 21 Ağustos 2019 tarihli görüntüler kullanılmıştır. Alana ait yangın risk modeli oluşturulmuş, ilaveten; bitki sağlığı izleme ve yanan alan tespiti için geliştirilen indeksler kullanılarak tahrip olmuş alanlar tespit edilmiştir. dNBR (Fark Normalize Yanma İndeksi) ve dNDVI (Fark Normalize Bitki İndeksi) indeksleri kullanılarak, kontrolsüz sınıflandırma işlemi uygulanmıştır. Sınıflandırılan görüntülerin alanları hesaplanarak doğruluk analizleri yapılmış ve Ekoloji Birliği verileriyle karşılaştırılmıştır. Yanan alan, elle oluşturulan vektör veri ile karşılaştırılmış Sentinel MSI verileriyle hesaplanan dNDVI ve dNBR indekslerinde sırasıyla % 99,96 ve % 99,95 doğrulukla hesaplanmıştır. Google Earth Engine platformuyla Sentinel 1 SAR görüntüleri; Sentinel 2 uydusuna ait dNDVI ve dNBR indeksleri üzerinde sınıflandırılan alanlara göre maskelenmiştir. Maskelenen alanlardan elde edilen saçılma değerleri istatistiksel olarak hesaplanarak elde edilen sonuçlar tartışılmıştır.",
+        models: [
+            "Orman Yangınları Sonrası Alan Tespiti için Uzaktan Algılama ve Yapay Zeka: İzmir'deki 2019 Yangını Örneği",
+            "İzmir Karabağlar Orman Yangınında Uzaktan Algılama Teknikleriyle Yanan Alanların Tespiti",
+            "İzmir Karabağlar Orman Yangınının Uzaktan Algılama ile Analizi ve Doğruluk Değerlendirmesi",
+            "Orman Yangınlarından sonra Uzaktan Algılama Teknikleri ile Alanın Belirlenmesi: İzmir Karabağlar Örneği"
         ]
     }
 ];
